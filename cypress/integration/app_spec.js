d599d2da465c46f0a3d6f37f997eb171e593821d
tamjeedhur <tamjeedhur@gmail.com>
2021-07-23 02:34:24 +0500

    challenge solved

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,28 +1,17 @@
 describe('Sign Up', () => {
   it('Adds person to course', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('input[name="name"]')
-      .click()
-      .type('Some Name')
-      .should('have.value', 'Some Name')
+    cy.get('input[name="name"]').click().type('Some Name').should('have.value', 'Some Name');
 
-    cy.get('input[name="email"]')
-      .click()
-      .type('some@email.com')
+    cy.get('input[name="email"]').click().type('some@email.com').should('have.value', 'some@email.com');
 
-    cy.get('select[name="department"]')
-      .select('core')
-      .should('have.value', 'core')
+    cy.get('select[name="department"]').select('core').should('have.value', 'core');
 
-    cy.get('select[name="course"]')
-      .select('git-it')
-      .should('have.value', 'git-it')
+    cy.get('select[name="course"]').select('git-it').should('have.value', 'git-it');
 
-    cy.get('input[type="submit"]')
-      .click()
+    cy.get('input[type="submit"]').click();
 
-    cy.get('li')
-      .should('contain', 'Some Name - some@email.com - core - git-it')
-  })
-})
+    cy.get('li').should('contain', 'Some Name - some@email.com - core - git-it');
+  });
+});
